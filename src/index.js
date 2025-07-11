a07dc22853efff2efabeb3330f7c8d73772b6385
Muhammad Faizan <m.fa1zan6002@gmail.com>
2025-07-11 10:09:53 +0500

    Fix the css and some basic issues

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 import StarRating from './StarRating';
 
-function Test(){
-  const [movie,setMovie] = useState(0)
+// function Test(){
+//   const [movie,setMovie] = useState(0)
 
-  return <>
-    <StarRating maxRating={10} onSetRating={setMovie} color='blue'/>
-    <p>This movie was rated {movie} rating</p>
-  </>
-}
+//   return <>
+//     <StarRating maxRating={10} onSetRating={setMovie} color='blue'/>
+//     <p>This movie was rated {movie} rating</p>
+//   </>
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+     <App />
 
-    <StarRating maxRating={5} message={["Terrible", "Bad", "Okay", "Good", "Amaizing"]} />
+    {/* <StarRating maxRating={5} message={["Terrible", "Bad", "Okay", "Good", "Amaizing"]} />
     <StarRating maxRating={5} color='red' size='50' defaultRating="3" />
-    <Test/>
+    <Test/> */}
   </React.StrictMode>
 );
