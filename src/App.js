4e5c99281c7116f2d3464a15ca68ca0bac84762f
Muhammad Faizan <164984334+mfa1zan@users.noreply.github.com>
2025-07-15 17:48:45 +0500

    fixed live api requests

--- src/App.js
+++ src/App.js
@@ -246,7 +246,7 @@ function MovieDetails({selectedId, onCloseMovie, onAddWatched, watched}){
   useEffect(function(){
     async function getMovieDetails(){
       setIsLoading(true)
-      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`)
+      const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`)
       const data = await res.json()
       console.log(data)
       setMovie(data)
